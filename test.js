90c7447eb7aa6293765d188d68965cef898ef586
Alex Fernandes <62030201+aafernands@users.noreply.github.com>
2022-08-04 10:59:55 -0400

    changes

---
+++ test.js
@@ -0,0 +1,28 @@
+const $box1 = $(".box1");
+const $box2 = $(".box2");
+const $box3 = $(".box3");
+const $box4 = $(".box4");
+const $box5 = $(".box5");
+
+function changeColor1() {
+	$box1.toggleClass("test1");
+	console.log("clicked1");
+}
+function changeColor2() {
+	$box2.toggleClass("test2");
+    console.log("clicked2");
+}
+function changeColor3() {
+	$box3.toggleClass("test3");
+    console.log("clicked3");
+
+}
+function changeColor4() {
+	$box4.toggleClass("test4");
+    console.log("clicked4");
+
+}
+function changeColor5() {
+	$box5.toggleClass("test5");
+    console.log("clicked5");
+}
